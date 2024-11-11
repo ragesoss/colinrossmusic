a19bc80552d70f127a7d1df1d51baf865b7d28b5
Sage Ross <ragesoss@gmail.com>
2024-11-11 10:05:19 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+	Amplitude.init({
+		songs: [
+			{
+				"name": "Judy O.",
+				"artist": "Colin Ross",
+				"album": "Refractions",
+				"url": "/music/refractions/1 - Judy O..mp3",
+				"cover_art_url": "/music/refraction/refractions-cover.jpg"
+			}
+	});
