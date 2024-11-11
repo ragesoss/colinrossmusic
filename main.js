117a21414b55cdb23a5d9fd8990c9366ebcae2c6
Sage Ross <ragesoss@gmail.com>
2024-11-11 10:09:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 				"name": "Judy O.",
 				"artist": "Colin Ross",
 				"album": "Refractions",
-				"url": "/music/refractions/1 - Judy O..mp3",
-				"cover_art_url": "/music/refraction/refractions-cover.jpg"
+				"url": "music/refractions/1 - Judy O..mp3",
+				"cover_art_url": "music/refraction/refractions-cover.jpg"
 			}
 	});
