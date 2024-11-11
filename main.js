c9644cc9ae439e05e08192f73293525575a65302
Sage Ross <ragesoss@gmail.com>
2024-11-11 10:48:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 	Amplitude.init({
-		songs: [
-			{
-				"name": "Judy O.",
-				"artist": "Colin Ross",
-				"album": "Refractions",
-				"url": "music/refractions/1 - Judy O..mp3",
-				"cover_art_url": "music/refraction/refractions-cover.jpg"
-			}
-		]
+  bindings: {
+    37: 'prev',
+    39: 'next',
+    32: 'play_pause'
+  },
+  debug: true,
+  visualization: 'michaelbromley_visualization',
+  songs: [
+	{
+		"name": "Judy O.",
+		"artist": "Colin Ross",
+		"album": "Refractions",
+		"url": "music/refractions/1 - Judy O..mp3",
+		"cover_art_url": "music/refraction/refractions-cover.jpg"
+	}
+  ],
+  waveforms: {
+      sample_rate: 100
+  },
+  visualizations: [
+    {
+      object: MichaelBromleyVisualization,
+      params: {
+
+      }
+    },
+    {
+      object: FrequencyAnalyzerVisualization,
+      params: {
+        type: 'dynamic'
+      }
+    }
+  ]
 	});
