10629ac70ce65eb755a83685a5a70dd9fb630c3c
Sage Ross <ragesoss@gmail.com>
2024-11-28 14:15:53 -0800

    Add Good For You album

---
+++ music/good-for-you/good-for-you.js
@@ -0,0 +1,147 @@
+	Amplitude.init({
+  bindings: {
+    37: 'prev',
+    39: 'next',
+    32: 'play_pause'
+  },
+  debug: true,
+  // visualization: 'michaelbromley_visualization',
+  songs: [
+    {
+      "name": "Swing Your Blues Away",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/1 - Swing Your Blues Away.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Nothing I Can Do",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/2 - Nothing I Can Do.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Red Dog Shuffle",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/3 - Red Dog Shuffle.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Love in Vain Blues",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/4 - Love in Vain Blues.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Open Road, Top Down",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/5 - Open Road, Top Down.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Good Mornin' Mr. Blues",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/6 - Good Mornin' Mr. Blues.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "She Thing",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/7 - She Thing.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Treat Me Right",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/8 - Treat Me Right.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Lie to You",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/9 - Lie to You.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Good for You",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/10 - Good for You.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Long Time Gone",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/11 - Long Time Gone.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Like You Used to Do",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/12 - Like You Used to Do.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Born in Little Rock",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/13 - Born in Little Rock.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "'Lectric Chair Blues",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/14 - 'Lectric Chair Blues.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Hold the Anchovies",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/15 - Hold the Anchovies.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Gypsy Time",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/16 - Gypsy Time.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    },
+    {
+      "name": "Movin' Through Me",
+      "artist": "Colin Ross Band",
+      "album": "Good For You",
+      "url": "/colinrossmusic/music/good-for-you/17 - Movin' Through Me.mp3",
+      "cover_art_url": "/colinrossmusic/music/good-for-you/cover.jpg"
+    }
+  ],
+  waveforms: {
+      sample_rate: 100
+  },
+  visualizations: [
+    {
+      object: MichaelBromleyVisualization,
+      params: {
+
+      }
+    },
+    {
+      object: FrequencyAnalyzerVisualization,
+      params: {
+        type: 'dynamic'
+      }
+    }
+  ]
+	});
