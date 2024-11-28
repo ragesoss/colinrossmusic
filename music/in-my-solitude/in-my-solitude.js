bfd2cc209d47401cc12c766ff374717d3246251f
Sage Ross <ragesoss@gmail.com>
2024-11-28 13:53:08 -0800

    Add In My Solitude album

---
+++ music/in-my-solitude/in-my-solitude.js
@@ -0,0 +1,140 @@
+	Amplitude.init({
+  bindings: {
+    37: 'prev',
+    39: 'next',
+    32: 'play_pause'
+  },
+  debug: true,
+  // visualization: 'michaelbromley_visualization',
+  songs: [
+    {
+      "name": "Track 1",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/1 - Track 1.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 2",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/2 - Track 2.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 3",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/3 - Track 3.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 4",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/4 - Track 4.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 5",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/5 - Track 5.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 6",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/6 - Track 6.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 7",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/7 - Track 7.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 8",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/8 - Track 8.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 9",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/9 - Track 9.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 10",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/10 - Track 10.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 11",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/11 - Track 11.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 12",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/12 - Track 12.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 13",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/13 - Track 13.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 14",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/14 - Track 14.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 15",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/15 - Track 15.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    },
+    {
+      "name": "Track 16",
+      "artist": "Colin Ross",
+      "album": "In My Solitude",
+      "url": "/colinrossmusic/music/in-my-solitude/16 - Track 16.mp3",
+      "cover_art_url": "/colinrossmusic/music/in-my-solitude/cover.jpg"
+    }
+  ],
+  waveforms: {
+      sample_rate: 100
+  },
+  visualizations: [
+    {
+      object: MichaelBromleyVisualization,
+      params: {
+
+      }
+    },
+    {
+      object: FrequencyAnalyzerVisualization,
+      params: {
+        type: 'dynamic'
+      }
+    }
+  ]
+	});
